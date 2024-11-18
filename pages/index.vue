a70da5dc06bad33af05929a8d3cd6fad697381c0
AbdulrahmanHadz <70710586+ArdaxHz@users.noreply.github.com>
2024-11-18 15:31:34 +0000

    use static prayer times value

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,47 @@ const props = defineProps({ mobile_break: Boolean })
 const adhan = useAdhanStore()
 const location = useLocationStore()
 const { currentPrayer, nextPrayer } = storeToRefs(adhan)
-const nearestPrayerTimes = ref(ref)
+const nearestPrayerTimes = ref({
+  data: [
+    {
+      masjid_id: '4624c1c5-b3d1-4314-b7e8-e3e03d22f429',
+      masjid_name: 'Attaqwa Mosque',
+      lat: 51.54174777,
+      long: 0.08471488,
+      dist_meters: 264.95190743,
+      next_prayer: 'fajr',
+      prayer_time: '2024-11-09T05:20:46+00:00',
+    },
+    {
+      masjid_id: 'f27bdd4b-0f61-4084-91c5-1fe0f32a387d',
+      masjid_name: 'Al-Madina Masjid',
+      lat: 51.5443534,
+      long: 0.077290535,
+      dist_meters: 769.59046919,
+      next_prayer: 'fajr',
+      prayer_time: '2024-11-09T05:15:46+00:00',
+    },
+    {
+      masjid_id: '67de7d8c-96ff-49f1-b9fd-a46963defd2f',
+      masjid_name: 'Masjed-e-Umar',
+      lat: 51.53568101,
+      long: 0.093294606,
+      dist_meters: 873.31244919,
+      next_prayer: 'fajr',
+      prayer_time: '2024-11-09T05:18:46+00:00',
+    },
+    {
+      masjid_id: 'df9309d9-1ead-487c-8c8a-5831d2b57a32',
+      masjid_name: 'Ilford Muslim Community Centre and Mosque',
+      lat: 51.55021469,
+      long: 0.080004931,
+      dist_meters: 995.28183959,
+      next_prayer: 'fajr',
+      prayer_time: '2024-11-09T05:14:46+00:00',
+    },
+  ],
+  count: 4,
+})
 const nearestMasjids = ref(null)
 const isLoading = ref(true)
 const isError = ref(false)
@@ -37,7 +77,7 @@ async function fetchData() {
         }),
       ])
 
-      nearestPrayerTimes.value = prayerTimesResponse.data
+      // nearestPrayerTimes.value = prayerTimesResponse.data
       nearestMasjids.value = nearestMasjidsResponse.data
     }
     catch (error) {
